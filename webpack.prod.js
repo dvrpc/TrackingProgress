65486fc21bc920e6fbf2671f2b84a83c4061df57
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2023-07-03 14:37:31 -0400

    add data to prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const config = require("./webpack.common");
 const { EnvironmentPlugin } = require("webpack");
+const CopyPlugin = require('copy-webpack-plugin')
 
 const prodConfig = {
   mode: "production",
@@ -8,6 +9,11 @@ const prodConfig = {
     new EnvironmentPlugin({
       markdownPath: "./markdown/",
       baseURL: "/TrackingProgress/"
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./data", to: "./data" }
+      ]
     }),
   ],
 };
